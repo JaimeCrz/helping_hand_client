0edf126e0f55ae7fefb971cbc0163deefcc350fc
Jaime <jaime.cruz.hostalot@gmail.com>
2020-04-14 15:54:35 +0200

    [TEST RED] Added test to see the total amount Co-authored-by: robin-lillqvist <robin.lillqvist@gmail.com> Co-authored-by: blake-futchi <blake@futchi.com>

--- cypress/integration/userCanAddProductsToRequest.feature.js
+++ cypress/integration/userCanAddProductsToRequest.feature.js
@@ -1,6 +1,6 @@
 describe("when products are visible", () => {
   before(() => {
-    //   cy.exec("yarn start")
+    cy.exec("yarn start")
     cy.server();
     cy.route({
       method: "GET",
@@ -8,15 +8,15 @@ describe("when products are visible", () => {
       response: "fixture:products.json",
     });
     cy.route({
-        method: "POST",
-        url: "http://localhost:3000/api/v1/tasks",
-        response: "fixture:taskList_response.json",
-      });
-      cy.route({
-        method: "PUT",
-        url: "http://localhost:3000/api/v1/tasks/1",
-        response: "fixture:taskList_update_response.json",
-      });
+      method: "POST",
+      url: "http://localhost:3000/api/v1/tasks",
+      response: "fixture:taskList_response.json",
+    });
+    cy.route({
+      method: "PUT",
+      url: "http://localhost:3000/api/v1/tasks/1",
+      response: "fixture:taskList_update_response.json",
+    });
     cy.visit("/");
   });
 
@@ -28,7 +28,7 @@ describe("when products are visible", () => {
       cy.get("button").should("contain", "Add").click();
     });
     cy.get("#request-list").within(() => {
-        cy.contains("Potatoes"); //product
+      cy.contains("Potatoes"); //product
     });
     cy.get("#product-2").within(() => {
       cy.contains("Shampoo"); //product
@@ -38,6 +38,9 @@ describe("when products are visible", () => {
     cy.get("#request-list").within(() => {
       cy.contains("Potatoes"); //product
       cy.contains("Shampoo"); //product
-  });
+    });
+    cy.get("#request-list").within(() => {
+      cy.get('total-price').should('contain', '228'); //product
+    });
   });
 });
